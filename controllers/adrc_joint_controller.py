44c9736370070ade883449053d8ffcfdd7f90bd0
JeremiaszWojciak <99556358+JeremiaszWojciak@users.noreply.github.com>
2023-05-13 14:44:06 +0200

    Update adrc_joint_controller.py

--- controllers/adrc_joint_controller.py
+++ controllers/adrc_joint_controller.py
@@ -1,6 +1,7 @@
 import numpy as np
 from observers.eso import ESO
 from .controller import Controller
+from models.manipulator_model import ManipulatorModel
 
 
 class ADRCJointController(Controller):
@@ -9,16 +10,40 @@ class ADRCJointController(Controller):
         self.kp = kp
         self.kd = kd
 
-        A = None
-        B = None
-        L = None
-        W = None
+        A = np.array([[0, 1, 0],
+                      [0, 0, 1],
+                      [0, 0, 0]])
+        B = np.array([[0],
+                      [self.b],
+                      [0]])
+        L = np.array([[3 * p],
+                      [3 * p ** 2],
+                      [p ** 3]])
+        W = np.array([[1, 0, 0]])
         self.eso = ESO(A, B, W, L, q0, Tp)
+        self.model = ManipulatorModel(Tp)
 
     def set_b(self, b):
         ### TODO update self.b and B in ESO
-        return NotImplementedError
+        self.b = b
+        B = np.array([[0],
+                     [self.b],
+                     [0]])
+        self.eso.set_B(B)
 
-    def calculate_control(self, x, q_d, q_d_dot, q_d_ddot):
+    def calculate_control(self, x, q_d, q_d_dot, q_d_ddot, i):
         ### TODO implement ADRC
-        return NotImplementedError
+        z_hat = self.eso.get_state()
+        f_hat = z_hat[2]
+        v = q_d_ddot + self.kd * (q_d_dot - z_hat[1]) + self.kp * (q_d - x[0])
+        u = (v - f_hat) / self.b
+
+        # do poprawy
+        if i == 1:
+            M_inv = np.linalg.inv(self.model.M([0.0, x[0], 0.0, x[1]])) # prędkość estymowana czy mierzona ?
+            new_b = M_inv[i, i]
+            self.set_b(new_b)
+            print(new_b)
+
+        self.eso.update(x[0], u)
+        return u
