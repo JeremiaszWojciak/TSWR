091705a98500d1952cebc0dc3b27b3f03286ff4c
JeremiaszWojciak <99556358+JeremiaszWojciak@users.noreply.github.com>
2023-05-13 14:43:16 +0200

    Update eso.py

--- observers/eso.py
+++ observers/eso.py
@@ -8,7 +8,7 @@ class ESO:
         self.B = B
         self.W = W
         self.L = L
-        self.state = np.pad(np.array(state), (0, A.shape[0] - len(state)))
+        self.state = np.pad(np.array(state), (0, A.shape[0] - len(state)))  # 1x3
         self.Tp = Tp
         self.states = []
 
@@ -18,6 +18,9 @@ class ESO:
     def update(self, q, u):
         self.states.append(copy(self.state))
         ### TODO implement ESO update
+        z_hat = self.state[:, np.newaxis]
+        z_dot_hat = self.A @ z_hat + self.B @ np.atleast_2d(u) + self.L @ (q - self.W @ z_hat)
+        self.state = self.state + self.Tp * z_dot_hat.flatten()
 
     def get_state(self):
         return self.state
